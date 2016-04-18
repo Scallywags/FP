d92a92c0841f96814e147807ed92d0c8e5609e4c
Jannyboy11 <jannyboy11@gmail.com>
2016-04-18 15:17:16 +0200

    recursive functions

--- lec1.hs
+++ lec1.hs
@@ -10,3 +10,19 @@ fun x y	| a>0 && b>0	= a + b
 			where
 				a = 3 * x + y
 				b = 3 * x
+
+fac :: (Eq a, Num a) => a -> a
+fac 0 = 1
+fac n = n * fac (n-1)
+
+mylength :: Num a => [b] -> a
+mylength []		= 0
+mylength (x:xs)	= 1 + mylength xs
+
+total :: Num a => [a] -> a
+total []		= 0
+total (x:xs)	= x + total xs
+
+mymap :: (a->b) -> [a] -> [b]
+mymap f []		= []
+mymap f (x:xs)	= f x : mymap f xs
