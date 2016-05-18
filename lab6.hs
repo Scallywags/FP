d8d94931ce300872ee8e99158456a419c06ade60
Jeroen W <jweener1996@gmail.com>
2016-05-18 11:05:05 +0200

    Lab 6 - Excercise 1 and 2

---
+++ lab6.hs
@@ -0,0 +1,20 @@
+module Lab6 where
+
+import FP_Grammar
+import FP_ParserGen
+import FP_TypesEtc
+
+myGrammar :: Grammar
+myGrammar nt = case nt of
+
+        Nmbr    -> [[ nmbr                                         ]]
+
+        Op      -> [[ op                                           ]]
+
+        Expr    -> [[ Expr, Op, Expr                               ]
+                   ,[ Nmbr                                         ]
+                   ,[ Var                                          ]
+                   ,[ lBracket, Expr, rBracket                     ]]
+                   
+        Stmnt   -> [[ Var, Terminal "=", Expr                      ]       -- Typecheck on op?
+                   ,[ Terminal "Repeat", Rep0 [Stmnt], Expr        ]]
