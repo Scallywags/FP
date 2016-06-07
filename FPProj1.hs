9f216b0231f731a71c8ca056e198ee6543adc117
Jeroen W <jweener1996@gmail.com>
2016-06-07 23:47:14 +0200

    Added documentation to exercise 1 and 2

--- FPProj1.hs
+++ FPProj1.hs
@@ -2,6 +2,20 @@ module Proj1 where
 
 import Data.List
 
+
+{- ===============================================================
+
+                            README
+                            
+ To evaluate an expression call evalProp (yourProgram) (yourQuery)
+                Example: evalProp program [C1]
+
+================================================================-}
+
+
+
+-- ============ Types ============
+
 data Atom 		= A0 | A1 | A2 | B0 | B1 | B2 | C0 | C1 | D deriving (Show, Eq)
 
 type Clause 	= (Atom, [Atom])
@@ -10,8 +24,22 @@ type Program	= [Clause]
 
 type Query		= [Atom]
 
-programme :: Program
-programme	= 	[(A0, [])
+
+-- The example program mentioned in the project documents
+{-
+    a0.                     - Should be true
+    a1.                     - Should be true
+    a2.                     - Should be true
+    
+    b0 :- a0, a1.           - Should be true
+    b1 :- a1, a2.           - Should be true
+    b2 :- a1, a2, d.        - Should be false (closed world assumption)
+    
+    c0 :- b0, b1.           - Should be true
+    c1 :- b0, b1, b2.       - Should be false
+-}
+program :: Program
+program	= 	[(A0, [])
 				,(A1, [])
 				,(A2, [])
 				,(B0, [A0, A1])
@@ -21,11 +49,59 @@ programme	= 	[(A0, [])
 				,(C1, [B0, B1, B2])
 				]
 
+
+-- This is the function that, when given a program and a query, can check if the propositions in the query are true.
 evalProp :: Program -> Query -> Bool
+
+-- If the program has no clauses we assume that our query is false since we live under the closed world assumption.
 evalProp [] _			= False
+
+-- We consider an empty query to be true.
 evalProp _ [] 			= True
+
+-- We check if every atom in our query has a clause in matches. If it hasn't, the query is false (closed world assumption).
 evalProp p qs 			| not $ all (\q -> q `elem` (map fst matches)) qs = False
+
+-- For every atom in the query we check if there is a clause in the program that by means of recursion eventually evaluates to true.
+-- This can be only one clause per atom, but all atoms in the query should have a match.
 						| otherwise = and [or [evalProp p as | (a, as) <- inner] | inner <- grouped]
 	where
+    
+        -- Matches is a list of all clauses in the given program that could be unified with an atom in the query
 		matches = [(a, as) | (a, as) <- p, q <- qs, q == a]
+        
+        -- Grouped groups the matches by atom in the query.
+        -- It returns a list consisting of lists.
+        -- The list has a list for every atom in the query and every
+        -- element in a list is a clause in the program that has a lefthandside equal to the atom of that list.
 		grouped = groupBy (\(a, as) (b, bs) -> a == b) matches
+
+
+-- ========== Test ==========
+
+-- Queries that should be true
+queries :: [Query]
+queries = [ [A0]
+          , [A1]
+          , [A2]
+          , [B0]
+          , [B1]
+          , [C0]
+          , [A0, A1, B0]
+          , [C0, B0, A2, C0, B0]
+          ]
+
+-- Queries that should be false
+falseQueries :: [Query]
+falseQueries = [ [D]
+               , [B2]
+               , [C1]
+               , [A0, D]
+               , [A1, B0, C0, C1]
+               , [C1, D]
+               ]
+
+-- The test itself. (This test tests program as defined above)
+test :: Bool
+test = (and $ map (evalProp program) queries) &&
+       (not $ or $ map (evalProp program) falseQueries)
--- FPProj2.hs
+++ FPProj2.hs
@@ -4,6 +4,22 @@ import Data.List
 import Data.Maybe
 import Data.Char
 
+{- ===============================================================
+
+                            README
+                            
+         To evaluate an expression you are given two options:
+         
+         1. Call evalOne (yourProgram) (yourQuery)
+            Example:    evalOne program [Atom "p", (Var "X")]
+            
+         2. Call parse (yourQueryAsAStringInPrologNotation)
+            Example:    parse "r(X)"
+
+================================================================-}
+
+
+
 -- ========== Types etc. ==========
 
 data Term	= Const String
@@ -14,38 +30,56 @@ type Pred 	= String
 data Atom 	= Atom Pred Term
 			deriving (Show, Eq)
 
+type Query	        = [Atom]
+
+type Clause	        = (Atom, [Atom])
+
+type Program	    = [Clause]
+
+type Substitution 	= (Term, Term)
+
+-- The solution we return consists of a boolean and a list of possible substitutions
+-- The boolean tells the user whether the query is true or not.
+type Solution       = (Bool, [Substitution])
+
+
+{- We want to be able to order atoms so that when we evaluate a query,
+   we first check the atoms containing a constant.
+   We do this because checking a constant on average takes less time than
+   checking a variable because then we need to deal with recursion.
+   When checking a query that would evaluate to false we now check it more
+   efficiently because we start checking the constants.
+-}
 instance Ord Atom where
 	compare (Atom _ (Var _)) (Atom _ (Var _))		= EQ 
 	compare (Atom _ (Const _)) (Atom _ (Const _))	= EQ
 	compare (Atom _ (Var _)) (Atom _ (Const _)) 	= GT 
 	compare (Atom _ (Const _)) (Atom _ (Var _)) 	= LT 
 
-type Query	= [Atom]
 
-type Clause	= (Atom, [Atom])
 
-type Program	= [Clause]
-
-type Substitution 	= (Term, Term)
 
 -- ========== Helper functions ==========
 
 isConst :: Term -> Bool
-isConst (Const _)	= True
-isConst _			= False
+isConst (Const _)	    = True
+isConst _			    = False
 
 isVar :: Term -> Bool
-isVar (Var _)		= True
-isVar _				= False
+isVar (Var _)		    = True
+isVar _				    = False
 
 predicate :: Atom -> Pred
-predicate (Atom p _)	= p
+predicate (Atom p _)    = p
 
 term :: Atom -> Term
-term (Atom _ t)	= t
+term (Atom _ t)	        = t
+
+
 
 -- ========== Expression type class ==========
 
+-- We defined "<=~" as the operator used for substituting
 class Expr e where
 	(<=~) :: e -> Substitution -> e
 	value :: e -> String
@@ -80,54 +114,80 @@ generateStrings = concat $ iterate (zipWith (++) aTOz) aTOz
 
 -- ========== Unify ==========
 
+-- Unify return a substitution of the first atom to the second atom if it finds any.
+-- When the first atom is a constant it can not find any substitutions because we should not substitute constants.
 unify :: Atom -> Atom -> Maybe Substitution
 unify (Atom p1 t1@(Var x))	(Atom p2 t2)	| p1 == p2	= Just (t1, t2)
 unify _ 					_ 				= Nothing
 
 -- ========== Evaluate ==========
 
-type Solution = (Bool, [Substitution])
-
+-- The function the user should be interested in.
+-- EvalOne generates solutions for a query given a program (or it doesn't if the query cannot be proven).
 evalOne :: Program -> Query -> Solution
 evalOne []		_						= (False, [])
 evalOne _		[]						= (True, [])
 evalOne prog	query 					= (solvable, substitutions)
 	where
+        -- We sort the atoms in the query. See above at Types etc. for an explanation why.
 		(q@(Atom p term):qs) = sort query
 		s = findSubstitutions prog prog q
 		(solvable', subs')	= evalOne prog qs
 
 		solvable = case term of
+            -- Solvable if substitutions can be found and the rest of the query is solvable.
 			Var _ 	-> 	(s /= []) && solvable'
+            
+            -- Solvable if the constant is the lefthandside of a clause in the program and the rest of the query is solvable
+            -- or if there exists a substitution where the lefthandside of a clause in the program can be replaced by this constant.
 			Const _	-> case q `elem` (map fst prog) of
-								True 	-> 	True
+								True 	-> 	if qs == [] then True else solvable'
 								False 	->	(Var "X", term) `elem` subsrec
 									where
 										varAtom = Atom p (Var "X")
 										(_, subsrec)	= evalOne prog [varAtom]
-
+                        
+                        -- When term is a variable we take the intersection of it's substitutions with substitutions found for the rest of the query.
 		substitutions 	| isVar term 	=	if qs == [] then s else intersect s subs'
+                        
+                        -- If not solvable, we return no substitutions.
 						| not solvable	= 	[]
+                        
+                        -- Otherwise (Constant and solvable) we return the substitutions of the rest of the query.
 						| otherwise		= 	subs'
 
 
+-- FindSubstitutions gets to programs. The first program is the whole program,
+-- the second program is a reduced program
 findSubstitutions :: Program -> Program -> Atom -> [Substitution]
 findSubstitutions _		[]										_						= []
 findSubstitutions _		_										(Atom _ (Const _))		= []
 findSubstitutions p 	((a@(Atom cpred cterm), as):cs)	q@(Atom qpred (Var s))	
 	| cpred == qpred	= subs subMaybe
+    -- If this clause is not applicable (different predicates) try again with the reduced program.
 	| otherwise			= findSubstitutions p cs q
 	where
+        -- Here we try to find a substitution for q so that it matches a.
 		subMaybe	= unify q a
+        
 		subs subM	= case subM of
-			
 			Just sub@(_, term) 	-> case term of
+                                    -- If the substitution found replaces a variable with
+                                    -- a constant the substitution should be returned along with the other
+                                    -- substitutions we might find.
 									Const _ 	-> sub : findSubstitutions p cs q
+                                    
+                                    -- If the substitution found replaces a variable with another variable
+                                    -- we try to find the substitutions for that variable.
 									Var _ 		-> snd (evalOne p as)
-
+                                    
+            -- If no substitution is found by unify we try again with the reduced program.
 			Nothing 			-> findSubstitutions p cs q
 
 
+-- ========== Test program ==========
+
+-- An extended version of the example program
 program :: Program
 program 	= 	[(Atom "p" (Const "a"), []) 										--p(a).
 				,(Atom "p" (Const "b"), [])											--p(b).
@@ -137,35 +197,59 @@ program 	= 	[(Atom "p" (Const "a"), []) 										--p(a).
                 ,(Atom "r" (Var "X"), [Atom "q" (Var "X")])                         --r(X) :- q(X).
 				,(Atom "s" (Var "X"), [Atom "p" (Var "X"), Atom "q" (Var "X")])		--s(X) :− p(X),q(X).
                 ,(Atom "t" (Var "X"), [Atom "p" (Var "X"), Atom "q" (Const "b")])   --t(X) :- p(X),q(b).
-                ,(Atom "t" (Var "X"), [Atom "p" (Var "a"), Atom "p" (Const "b")])   --t(X) :- p(a),p(b).
                 ,(Atom "u" (Var "X"), [Atom "p" (Const "c"), Atom "r" (Var "X")])   --u(X) :- p(c),r(X).
                 ,(Atom "v" (Const "a"), [Atom "q" (Var "X"), Atom "p" (Const "c"), Atom "r" (Var "X"), Atom "s" (Var "X"), Atom "q" (Const "b")]) 	--v(a) :- q(X),p(c),r(X),s(X),q(b).
                 ,(Atom "w" (Var "Y"), [Atom "r" (Var "Y")])							--w(Y) :- r(Y).
 				]
-                
-query :: Int -> Query
-query 0     = [Atom "p" (Const  "a")]   --p(a)?
-query 1     = [Atom "p" (Const  "c")]   --p(c)?
-query 2     = [Atom "q" (Var    "X")]   --q(X)?
-query 3     = [Atom "r" (Const  "a")]   --r(a)?
-query 4     = [Atom "r" (Var    "X")]   --r(X)?
-query 5     = [Atom "r" (Var    "A")]   --r(A)?
-query 6     = [Atom "s" (Var    "X")]   --s(X)?
-query 7     = [Atom "s" (Const  "a")]   --s(a)?
-query 8     = [Atom "t" (Var    "X")]   --t(X)?
-query 9     = [Atom "t" (Const  "a")]   --t(a)?
-query 10    = [Atom "u" (Const  "a")]   --u(a)?
-query 11    = [Atom "u" (Var    "X")]   --u(X)?
-query 12    = [Atom "v" (Var    "X")]   --v(X)?
-query 13    = [Atom "v" (Var    "Y")]   --v(Y)?
-query 14    = [Atom "v" (Const  "a")]   --v(a)?
+
+-- ========== Test ==========
 
 queries :: [Query]
-queries = [query x | x <- [0..14]]
+queries = [ [Atom "p" (Const  "a")]   --p(a)?
+          , [Atom "p" (Const  "b")]   --p(b)?
+          , [Atom "p" (Const  "c")]   --p(c)?
+          , [Atom "q" (Var    "X")]   --q(X)?
+          , [Atom "r" (Const  "a")]   --r(a)?
+          , [Atom "r" (Var    "X")]   --r(X)?
+          , [Atom "r" (Var    "A")]   --r(A)?
+          , [Atom "s" (Var    "X")]   --s(X)?
+          , [Atom "s" (Const  "a")]   --s(a)?
+          , [Atom "t" (Var    "X")]   --t(X)?
+          , [Atom "t" (Const  "a")]   --t(a)?
+          , [Atom "u" (Const  "a")]   --u(a)?
+          , [Atom "u" (Var    "X")]   --u(X)?
+          , [Atom "v" (Var    "X")]   --v(X)?
+          , [Atom "v" (Var    "Y")]   --v(Y)?
+          , [Atom "v" (Const  "a")]   --v(a)?
+          ]
 
-test :: Program -> Bool
-test p = and [fst $ evalOne p q | q <- queries]
+falseQueries :: [Query]
+falseQueries = [ [Atom "p" (Const "d")]
+               , [Atom "a" (Const "a")]
+               , [Atom "r" (Const "c")]
+               , [Atom "s" (Const "c")]
+               , [Atom "t" (Const "d")]
+               , [Atom "u" (Const "c")]
+               , [Atom "v" (Const "b")]
+               , [Atom "w" (Const "c")]
+               , [Atom "p" (Const "a"), Atom "p" (Const "d"), Atom "p" (Var "X")] -- This one fails !
+               , [Atom "p" (Var   "X"), Atom "q" (Const "c")]
+               , [Atom "p" (Const "d"), Atom "p" (Var   "X")]
+               , [Atom "p" (Var   "X"), Atom "a" (Var   "X")]
+               ]
 
+test :: Bool
+test = (and $ map fst $ map (evalOne program) queries) &&
+       (not $ or $ map fst $ map (evalOne program) falseQueries)
+       
+       
+-- ========== Parser ==========
+{- We made a parser that recognizes atoms like they are defined in prolog. (But only one character for predicates and constants and variables!)
+   This method immediately calls the evalOne function so you only have to type something like:
+        Example: parse "r(X)". The result is "(True,[(Var "X", Const "a"), (Var "X", Const "b")])"
+        Example: parse "p(a)". The result is "(True, [])"
+   NB:  This parser only recognizes queries which consist of a single atom.
+-}
 parse :: String -> Solution
 parse (p:'(':c:')':[])  | isLower c = evalOne program [Atom [p] (Const [c])]
                         | otherwise = evalOne program [Atom [p] (Var [c])]
